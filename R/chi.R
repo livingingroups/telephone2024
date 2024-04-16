a71fbfbf568d8f4e1b22515e9f524f559345bbad
Chi Hsin Chen <chi.chen@lilo.org>
2024-04-16 14:20:27 +0200

    updated chi_function()

--- R/chi.R
+++ R/chi.R
@@ -1,3 +1,12 @@
 chi_function <- function(word){
-  return(word)
-}
+  input_word<-which(all_english_words==word)
+  similar_word<-all_english_words[input_word-1]
+
+  if (length(similar_word)>0)
+  return(similar_word)
+
+
+  else if (length(input_word)==0)
+  return(substr(word, 1,4))
+         }
+
